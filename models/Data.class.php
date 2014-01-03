78b15d1f9c23582b89fd69bdd93d7d6d46d4e539
kideh88 <kim.dehmlow@gmail.com>
2014-01-03 12:37:40 +0100

    - Added connection setup

---
+++ models/Data.class.php
@@ -0,0 +1,21 @@
+<?php
+class Data {
+    private $objPDO;
+
+    public function __construct($arrConfig) {
+        try {
+            $objConnection = new PDO('mysql:host=' . $arrConfig['server'] . '; dbname=' . $arrConfig['name'], $arrConfig['user'], $arrConfig['password']);
+            $objConnection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            $objConnection->exec("SET CHARACTER SET utf8"); // return all sql requests as UTF-8
+            $this->objPDO = $objConnection;
+        }
+        catch (PDOException $err) {
+            return json_encode(array('success' => false, 'error' => "Failed to construct PDO Connection"));
+        }
+    }
+
+    public function pdo() {
+        return $this->objPDO;
+    }
+
+}
--- models/Discuss.class.php
+++ models/Discuss.class.php
@@ -2,10 +2,14 @@
 
 class Discuss {
     public $_strBasePath;
+    public $_objDataClass;
 
     public function __construct() {
         $this->_strBasePath = $_SERVER['DOCUMENT_ROOT'] . '/discuss';
+
         require_once($this->_strBasePath . '/config/config.inc.php');
+        $this->_objDataClass = new Data($arrConnectionConfig);
+
     }
 
 }
